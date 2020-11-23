77e53da84e16bfa2ec5a9a45413e5f2de424f584
georges sysaath <georges.sysaath@gmail.com>
2020-11-23 17:08:21 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :recipes
+  resources :cookbooks
+  resources :groceries, except: [ :destroy ]
+  resources :weeks, except: [ :destroy ] do
+    member do
+      get :today
+    end
+    resources :meals, except: [ :destroy, :index ]
+  end
 end
