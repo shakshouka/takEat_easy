287fe82822aa1e9b3303813a17bc67218b7a69c1
Olivier Calmels <calmels.o@gmail.com>
2020-11-23 19:40:14 +0100

    seed ingredients & images in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'nokogiri'
+
+puts "######################################"
+puts "########## SEED - START ##############"
+puts "######################################"
+
+# Ingredients
+
+("a".."z").each do |letter| 
+  url = "https://www.marmiton.org/recettes/index/ingredient/#{letter}"
+
+  html_file = open(url).read
+  html_doc = Nokogiri::HTML(html_file)
+
+  html_doc.search('.index-item-card').each do |element|
+    text = element.search('.index-item-card-name')
+    puts text.text.strip
+    img = element.search('img')
+    p img.attr('src').value
+  end
+end
