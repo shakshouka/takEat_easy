a45c453c64154b918367d3377fceb9d24495ec48
Alexandre Wotawa <awotawa@gmail.com>
2020-11-24 18:14:35 +0100

    added a background image for the display of cookbooks and created a partial scss file for the cookbook cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,13 +19,12 @@ user = User.create!(email: 'toto@yahoo.fr', password: 'abcdef', password_confirm
 user1 = User.create!(email: 'toto1@yahoo.fr', password: 'abcdef', password_confirmation: 'abcdef', first_name: "Martin", last_name: "Tran", address: "21 Rue de l'École de Médecine, 75006 Paris")
 p "-------------"
 p "Recipes creation"
-recipe = Recipe.create!(name: 'Omelette', instructions: 'Cassez 2 oeufs et puis YOLO!! Filler filler filler filler filler filler filler filler filler filler filler filler YOOOOOooooooooolooooooooooooooooooooooooooooooooooooooo', difficulty: 'facile', cooking_time: 5, user_id: 1)
-p "HELLOOOOOOOOOOO"
-recipe1 = Recipe.create!(name: 'OmeletteBis', instructions: 'Cassez 2 oeufs et puis YOLO!! Filler filler filler filler filler filler filler filler filler filler filler filler YOOOOOooooooooolooooooooooooooooooooooooooooooooooooooo', difficulty: 'facile', cooking_time: 5, user_id: 1)
+recipe = Recipe.create!(name: 'Omelette', instructions: 'Cassez 2 oeufs et puis YOLO!! Filler filler filler filler filler filler filler filler filler filler filler filler YOOOOOooooooooolooooooooooooooooooooooooooooooooooooooo', difficulty: 'Facile', cooking_time: 5, user_id: 1)
+recipe1 = Recipe.create!(name: 'OmeletteBis', instructions: 'Cassez 2 oeufs et puis YOLO!! Filler filler filler filler filler filler filler filler filler filler filler filler YOOOOOooooooooolooooooooooooooooooooooooooooooooooooooo', difficulty: 'Facile', cooking_time: 5, user_id: 1)
 p "-------------"
 p "Cookbooks creation"
-cookbook = Cookbook.create!(name: 'Hello', user_id: 1)
-cookbook1 = Cookbook.create!(name: 'world', user_id: 1)
+cookbook = Cookbook.create!(name: 'Hello', description: 'Awesome beginner starter', user_id: 1)
+cookbook1 = Cookbook.create!(name: 'World', description: 'Second best book to have!', user_id: 1)
 p "-------------"
 p "*************************"
 p "****  DB:SEED END !  ****"
