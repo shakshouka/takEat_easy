f5393563cb052fd45d9168da38541fccc45ee243
Olivier Calmels <calmels.o@gmail.com>
2020-11-25 12:33:27 +0100

    Remove colums units from doses AND Added column unit to doses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_112737) do
+ActiveRecord::Schema.define(version: 2020_11_25_113232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 2020_11_25_112737) do
     t.float "quantity"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "units"
+    t.string "unit"
     t.index ["ingredient_id"], name: "index_doses_on_ingredient_id"
     t.index ["recipe_id"], name: "index_doses_on_recipe_id"
   end
