30cbede39c53bc06b765cc2bb96cac6fcfcf1038
Alexandre Wotawa <awotawa@gmail.com>
2020-11-25 13:54:26 +0100

    implemented a swiper for the app and created a swipe page to test it out in pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/random', to: "pages#random"
+  get '/swipe', to: "pages#swipe"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :recipes
   resources :cookbooks
