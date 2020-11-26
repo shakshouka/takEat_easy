27283179a964465100f65d7b9e59ab45a0453eb5
georges sysaath <georges.sysaath@gmail.com>
2020-11-26 08:48:26 +0100

    if user signed in he cant see home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    if user_signed_in?
+      redirect_to 'weeks/1/today'
+    end
   end
 
   def random
