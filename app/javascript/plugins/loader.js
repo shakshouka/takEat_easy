ba60dfbe73342ddb44e0709b9b0d1b2f7f5c81d4
georges sysaath <georges.sysaath@gmail.com>
2020-12-01 15:23:57 +0100

    loader

--- app/javascript/plugins/loader.js
+++ app/javascript/plugins/loader.js
@@ -2,7 +2,7 @@ const loader = () => {
   if (document.querySelector("#loader")){
     setTimeout(function(){
       window.location.replace("http://localhost:3000/weeks/1")
-    }, 4000)}
+    }, 2000)}
 };
 
 export { loader }
