bd57808099f0dc9f29e2ec62d6b8518cf9c22d74
Alexandre Wotawa <awotawa@gmail.com>
2020-12-01 10:52:11 +0100

    fixed the js in case the list  doesn't exist yet

--- app/javascript/plugins/grocery-org.js
+++ app/javascript/plugins/grocery-org.js
@@ -1,11 +1,13 @@
 const grorg = () => {
   // Add a "checked" symbol when clicking on a list item
   var list = document.querySelector('#myUL');
-  list.addEventListener('click', function(ev) {
-    if (ev.target.tagName === 'LI') {
-      ev.target.classList.toggle('checked');
-    }
-  }, false);
+  if (list) {
+    list.addEventListener('click', function(ev) {
+      if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+      }
+    }, false);
+  }
 };
 
 export { grorg }
