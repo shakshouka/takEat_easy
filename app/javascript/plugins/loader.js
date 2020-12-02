10bbc6fd49fd1438e5d4fd440192430bba14baa4
Olivier Calmels <calmels.o@gmail.com>
2020-12-02 10:17:16 +0100

    load

--- app/javascript/plugins/loader.js
+++ app/javascript/plugins/loader.js
@@ -1,8 +1,8 @@
 const loader = () => {
   if (document.querySelector("#loader")){
     setTimeout(function(){
-      window.location.replace("http://google.com")
-    }, 4000)}
+      window.location.replace("http://www.takeateasy.club/weeks/1")
+    }, 2000)}
 };
 
 export { loader }
