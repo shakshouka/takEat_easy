61cb83b8a3b005763e424351472b3886e68ac0d6
Olivier Calmels <calmels.o@gmail.com>
2020-12-02 16:19:38 +0100

    Naked domain is redirected to www Title tag is set and exceeds 20 chars Favicon tag is set Open Graph meta tags are properly set 404 and 500 pages are customized og:image has the right dimensions (1200x630)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,4 +110,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+# Route exceptions to the application router vs. default
+config.exceptions_app = self.routes
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,16 @@ Rails.application.routes.draw do
   get '/swipe', to: "pages#swipe"
   get 'user', to: "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  # error pages
+  %w( 404 422 500 503 ).each do |code|
+    get code, :to => "errors#show", :code => code
+  end
+  
   resources :recipes do
     resources :cookbook_recipes, only: [ :create ]
   end
+
   resources :cookbook_recipes, only: [ :destroy ]
   resources :cookbooks
   resources :grocery_items, only: [ :update ]
