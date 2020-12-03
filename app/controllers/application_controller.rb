e62cff82bffa410fc68183a095b0e4ed901b8f58
Olivier Calmels <calmels.o@gmail.com>
2020-12-03 10:41:29 +0100

    redirect and error pages must be ok. lets try this on heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,18 @@
 class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
-  before_action :add_www_subdomain
 
   include Pundit
 
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
-  def add_www_subdomain
-    unless /^www/.match(request.host) || request.host == "localhost"
-      redirect_to("#{request.protocol}www.takeateasy.club#{request.fullpath}",
-                  :status => 301)
-    end
-  end
-
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
-  
+
     # Make sure your production DOMAIN variable is set with your domain name. If you deploy your code with Heroku for instance, just type in your terminal:
 
-    # heroku config:set DOMAIN=www.my_product.com
+    # heroku config:set DOMAIN=www.takeateasy.club
 
     #You can check it's properly set with heroku config:get DOMAIN.
 
